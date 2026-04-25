a966bd44479a0d04987dd185357838c5da148c23
Agusti Peinado <agustipeinado46@gmail.com>
2026-04-25 19:55:19 +0200

    Add Terraform infraestructure

---
+++ infra/terraform/main.tf
@@ -0,0 +1,34 @@
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = var.acr_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+
+resource "azurerm_service_plan" "plan" {
+  name                = "plan-devops-cloud-lab-tf"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  os_type             = "Linux"
+  sku_name            = "B1"
+}
+
+resource "azurerm_linux_web_app" "app" {
+  name                = var.app_name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  service_plan_id     = azurerm_service_plan.plan.id
+
+  site_config {
+    application_stack {
+      docker_image     = "cloudlab-api"
+      docker_image_tag = "latest"
+    }
+  }
+}
---
+++ infra/terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "location" {
+  default = "westeurope"
+}
+
+variable "resource_group_name" {
+  default = "rg-devops-cloud-lab-tf"
+}
+
+variable "app_name" {
+  default = "app-devops-cloud-lab-peinado-tf"
+}
+
+variable "acr_name" {
+  default = "acrdevopscloudlabtf"
+}
